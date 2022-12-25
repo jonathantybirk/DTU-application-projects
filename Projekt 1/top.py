fbc50dd6b3e785ae26320bf359238e61daca5d58
jonathantybirk <79806681+jonathantybirk@users.noreply.github.com>
2022-12-25 23:10:40 +0100

    Add files via upload

---
+++ Projekt 1/top.py
@@ -0,0 +1,12 @@
+import pygame as pg
+import random as rand
+
+# Application
+appDim = (1600,900)
+backgroundColor = (15,15,20)
+caption = "Collisions"
+
+# Functional
+app = pg.display.set_mode(appDim)
+pg.display.set_caption(caption)
+EXIT = False
